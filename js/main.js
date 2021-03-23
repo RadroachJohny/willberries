e697dc7767b0e8f8e9f2fbd023b6bd07a69fdddd
Igor Lait <radroachjohny@gmail.com>
2021-03-23 17:33:11 +0300

    turn on modal and to top btn

--- js/main.js
+++ js/main.js
@@ -10,5 +10,46 @@ const mySwiper = new Swiper('.swiper-container', {
 
 // CART
 
-console.log(2);
+const buttonCart = document.querySelector('.button-cart'),
+      modalCart = document.getElementById('modal-cart'),
+      modalClose = document.querySelector('.modal-close');
+
+const keyDownCheck = (e) => {  
+    console.log(e.key);
+    if (e.which === 27) {
+      closeModal();
+    }  
+};
+
+const openModal = () => {
+  modalCart.classList.add('show');
+  document.addEventListener('keydown', keyDownCheck);
+};
+
+const closeModal = () => {
+  modalCart.classList.remove('show');
+  document.removeEventListener('keydown', keyDownCheck);
+};
+
+buttonCart.addEventListener('click', openModal);
+
+
+// scroll smooth
+const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+(function() {
+  for (let i = 0; i < scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = scrollLinks[i].getAttribute('href');
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+  
+    });
+  }
+})();
+
+
 
